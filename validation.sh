70c9f9097b7216a109e8b0647b224eef51c8b586
moriya <c0115334ef@edu.teu.ac.jp>
2018-10-20 12:31:21 +0000

    add shell script for validation

---
+++ validation.sh
@@ -0,0 +1,28 @@
+pipenv shell
+cd ~/work/topic-transfer/src/
+case $HOSTNAME in
+  "clover11" ) DATA=yelpf;;
+  "clover12" ) DATA=db_pedia ;;
+  "denso" ) DATA=ag_news ;;
+esac
+echo $DATA
+for i in `seq $1 2 4`
+do
+  echo python topic-train-soft.py -m $DATA -g $1 -d 17 -v $i
+  python topic-train-soft.py -m $DATA -g $1 -d 17 -v $i
+  echo python transfer-soft.py -m $DATA -g $1 -d 17 -v $i
+  python transfer-soft.py -m $DATA -g $1 -d 17 -v $i
+done
+START=$((($1+1)%2))
+for i in `seq $START 2 4`
+do
+  echo python topic-train-hard.py -m $DATA -g $1 -d 17 -v $i
+  python topic-train-hard.py -m $DATA -g $1 -d 17 -v $i
+  echo python transfer-hard.py -m $DATA -g $1 -d 17 -v $i
+  python transfer-hard.py -m $DATA -g $1 -d 17 -v $i
+done
+for i in `seq $1 2 4`
+do
+  echo python train-scratch.py -m $DATA -g $1 -d 17 -v $i
+  python train-scratch.py -m $DATA -g $1 -d 17 -v $i
+done
