48298f0ef187f187c0fe4e36a8b0e62ce5dfa8d1
moriya <c0115334ef@edu.teu.ac.jp>
2018-10-30 05:49:13 +0000

    add exec python

--- multi-validation.sh
+++ multi-validation.sh
@@ -8,5 +8,7 @@ echo mode=$DATA gpu_id=$1
 for i in `seq $1 2 4`
 do
   echo python topic-train-multi-3.py -m $DATA -g $1 -d 17 -v $i
+  python topic-train-multi-3.py -m $DATA -g $1 -d 17 -v $i
   echo python transfer-multi-3.py -m $DATA -g $1 -d 17 -v $i
+  python transfer-multi-3.py -m $DATA -g $1 -d 17 -v $i
 done
