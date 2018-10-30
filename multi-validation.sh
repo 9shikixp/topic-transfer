5948a6a488cdc08083cc0d3c9daa7580fd1d4b29
moriya <c0115334ef@edu.teu.ac.jp>
2018-10-30 05:37:11 +0000

    add script for multi transfer

---
+++ multi-validation.sh
@@ -0,0 +1,12 @@
+cd ~/work/topic-transfer/src/
+case $HOSTNAME in
+  "clover11" ) DATA=yelpf;;
+  "clover12" ) DATA=db_pedia ;;
+  "denso" ) DATA=ag_news ;;
+esac
+echo mode=$DATA gpu_id=$1
+for i in `seq $1 2 4`
+do
+  echo python topic-train-multi-3.py -m $DATA -g $1 -d 17 -v $i
+  echo python transfer-multi-3.py -m $DATA -g $1 -d 17 -v $i
+done
