637277a6975ab07894774bee55785f531cf95cec
moriya <c0115334ef@edu.teu.ac.jp>
2018-11-02 05:20:01 +0000

    multi-4 topic transfer (n_topic=[20, 100, 200, kind*10])

---
+++ src/topic-train-multi-4.py
@@ -0,0 +1,202 @@
+import argparse
+from collections import Counter
+import pandas as pd
+
+import numpy as np
+import chainer
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+
+import os, sys
+sys.path.append(os.pardir+'/src')
+from vdcnn_multi_4 import MultiVDCNN
+
+
+def lossfun(x, t):
+    with cuda.get_device(t):
+        soft20_x = x[:,:20]
+        soft100_x = x[:,20:120]
+        soft200_x = x[:,120:320]
+        soft10n_x = x[:,320:]
+        soft20_label = t[:,1:21]
+        soft100_label = t[:,21:121]
+        soft200_label = t[:,121:321]
+        soft10n_x = x[:,321:]
+
+    soft20_loss = - (F.sum(soft20_label * F.log_softmax(soft20_x))) / soft20_x.shape[0]
+    soft100_loss = - (F.sum(soft100_label * F.log_softmax(soft100_x))) / soft100_x.shape[0]
+    soft200_loss = - (F.sum(soft200_label * F.log_softmax(soft200_x))) / soft200_x.shape[0]
+    soft10n_loss = - (F.sum(soft10n_label * F.log_softmax(soft10n_x))) / soft10n_x.shape[0]
+    loss = soft20_loss + soft100_loss + soft200_loss + soft10n_loss
+    return loss
+
+def accfun(x, t):
+    with cuda.get_device(t):
+        label = t[:,0].astype('int32')
+        soft100_x = x[:,20:120]
+    return F.accuracy(soft100_x, label)
+
+
+parser = argparse.ArgumentParser(description='Train from scratch.')
+parser.add_argument('--mode', '-m', dest='mode',default='ag_news',
+                    help='use dataset (ag_news, db_pedia, yelpf)')
+parser.add_argument('--gpu', '-g', dest='gpu', default=0, type=int,
+                    help='gpu_id (0 or 1)')
+parser.add_argument('--depth', '-d', dest='depth', default=17, type=int,
+                    help='conv depth (9, 17, 29)')
+parser.add_argument('--valid', '-v', dest='valid', default=0, type=int,
+                    help='valid id')
+
+args = parser.parse_args()
+
+
+mode = args.mode
+
+if mode == 'ag_news':
+    train_df = pd.read_csv('../data/ag_news/topic100_train.csv')
+    test_df = pd.read_csv('../data/ag_news/topic100_test.csv')
+    drop_header = ['class_id', 'title', 'description']
+    train_soft20 = pd.read_csv('../data/ag_news/topic20_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft20 = pd.read_csv('../data/ag_news/topic20_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft200 = pd.read_csv('../data/ag_news/topic200_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft200 = pd.read_csv('../data/ag_news/topic200_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft10n = pd.read_csv('../data/ag_news/topic40_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft10n = pd.read_csv('../data/ag_news/topic40_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+
+elif mode == 'db_pedia':
+    train_df = pd.read_csv('../data/db_pedia/topic100_train.csv')
+    test_df = pd.read_csv('../data/db_pedia/topic100_test.csv')
+    drop_header = ['class_id', 'title', 'description']
+    train_soft20 = pd.read_csv('../data/db_pedia/topic20_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft20 = pd.read_csv('../data/db_pedia/topic20_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft200 = pd.read_csv('../data/db_pedia/topic200_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft200 = pd.read_csv('../data/db_pedia/topic200_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft10n = pd.read_csv('../data/db_pedia/topic140_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft10n = pd.read_csv('../data/db_pedia/topic140_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+
+elif mode == 'yelpf':
+    train_df = pd.read_csv('../data/yelp_review_full/topic100_train.csv')
+    test_df = pd.read_csv('../data/yelp_review_full/topic100_test.csv')
+    drop_header = ['class_id', 'review']
+    train_soft20 = pd.read_csv('../data/yelp_review_full/topic20_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft20 = pd.read_csv('../data/yelp_review_full/topic20_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft200 = pd.read_csv('../data/yelp_review_full/topic200_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft200 = pd.read_csv('../data/yelp_review_full/topic200_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    train_soft10n = pd.read_csv('../data/yelp_review_full/topic50_train.csv').drop(drop_header, axis=1).values.astype(np.float32)
+    test_soft10n = pd.read_csv('../data/yelp_review_full/topic50_test.csv').drop(drop_header, axis=1).values.astype(np.float32)
+
+
+if mode == 'ag_news' or mode == 'db_pedia':
+    train_df.title = train_df.title.str.lower()
+    train_df.description = train_df.description.str.lower()
+    test_df.title = test_df.title.str.lower()
+    test_df.description = test_df.description.str.lower()
+elif mode == 'yelpf':
+    train_df.review = train_df.review.str.lower()
+    test_df.review = test_df.review.str.lower()
+
+
+characters = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/|_#$%^&*~`+=<>()[]{}\t "
+char2id = {}
+for i, c in enumerate(characters):
+    char2id[c] = i
+
+    
+if mode == 'ag_news' or mode == 'db_pedia':
+    train_x = train_df.title.values + ' ' + train_df.description.values
+    test_x = test_df.title.values + ' ' + test_df.description.values
+elif mode == 'yelpf':
+    train_x = train_df.review.values
+    test_x = test_df.review.values
+
+train_x = [[char2id[c] if c in char2id else len(char2id) for c in s][:1024] for s in train_x]
+test_x = [[char2id[c] if c in char2id else len(char2id) for c in s][:1024] for s in test_x]
+
+f = lambda x: np.pad(x, pad_width=(0, 1024-len(x)), mode='constant', constant_values=-1)
+train_x = np.array(list(map(f, train_x)), np.int32)
+test_x = np.array(list(map(f, test_x)), np.int32)
+
+if mode == 'ag_news' or mode == 'db_pedia':
+    drop_header = ['class_id', 'title', 'description']
+elif mode == 'yelpf':
+    drop_header = ['class_id', 'review']
+
+train_y = train_df.drop(drop_header, axis=1).values.argmax(axis=1).astype(np.int32)
+test_y = test_df.drop(drop_header, axis=1).values.argmax(axis=1).astype(np.int32)
+train_soft = train_df.drop(drop_header, axis=1).values.astype(np.float32)
+test_soft = test_df.drop(drop_header, axis=1).values.astype(np.float32)
+
+n_out1 = train_soft20.shape[1]
+n_out2 = train_soft.shape[1]
+n_out3 = train_soft200.shape[1]
+n_out4 = train_soft10n.shape[1]
+
+depth = args.depth
+if depth == 9:
+    depth = [1, 1, 1, 1]
+elif depth == 17:
+    depth = [2, 2, 2, 2]
+elif depth == 29:
+    depth = [5, 5, 2, 2]
+else:
+    print('depth must be (9, 17, 29)')
+    sys.exit()
+
+print('MultiVDCNN setting: emb_dim={} n_out_1={}, n_out_2={}, n_out_3={}, n_out_4={}, depth={}'.format(len(char2id)+1, n_out1, n_out2, n_out3, n_out4, sum(depth)*2+1))
+
+gpu_id = args.gpu
+
+model = MultiVDCNN(len(char2id)+1, n_out1, n_out2, n_out3, n_out4, depth)
+if gpu_id >= 0:
+    model.to_gpu(gpu_id)
+
+
+train_y = np.concatenate([train_y.reshape(-1, 1), train_soft20, train_soft40, train_soft, train_soft200], axis=1).astype(np.float32)
+test_y = np.concatenate([test_y.reshape(-1, 1), test_soft20, test_soft40, test_soft, test_soft200], axis=1).astype(np.float32)
+
+print(mode, train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+
+train = datasets.TupleDataset(train_x, train_y)
+
+test = datasets.TupleDataset(test_x, test_y)
+
+batch_size = 128
+
+train_iter = iterators.SerialIterator(train, batch_size)
+test_iter = iterators.SerialIterator(test, batch_size, False, False)
+
+epoch_size = 5000
+max_epoch = 15
+
+model = L.Classifier(model, lossfun=lossfun, accfun=accfun)
+
+optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)
+
+optimizer.setup(model)
+
+updater = training.updaters.StandardUpdater(train_iter, optimizer, device=gpu_id)
+
+result_dir = '../results/multi4_softtopic_{}_{}_{}_{}_{}_depth{}_valid{}'.format(mode, n_out1, n_out2, n_out3, n_out4, sum(depth)*2+1, args.valid)
+trainer = training.Trainer(updater, (epoch_size * max_epoch, 'iteration'), out=result_dir)
+
+from chainer.training import extensions
+
+trainer.extend(extensions.LogReport(trigger=(epoch_size, 'iteration')))
+trainer.extend(extensions.snapshot(filename='snapshot_iteration-{.updater.iteration}'), trigger=(epoch_size, 'iteration'))
+trainer.extend(extensions.snapshot_object(model.predictor, filename='model_iteration-{.updater.iteration}'), trigger=(epoch_size, 'iteration'))
+trainer.extend(extensions.Evaluator(test_iter, model, device=gpu_id), trigger=(epoch_size, 'iteration'))
+trainer.extend(extensions.observe_lr(), trigger=(epoch_size, 'iteration'))
+trainer.extend(extensions.PrintReport(['iteration', 'lr', 'main/accuracy', 'validation/main/accuracy', 'elapsed_time']), trigger=(epoch_size, 'iteration'))
+trainer.extend(extensions.dump_graph('main/loss'))
+trainer.extend(extensions.ExponentialShift('lr', 0.5), trigger=(epoch_size*3, 'iteration'))
+trainer.extend(extensions.ProgressBar(update_interval=30))
+
+print('running')
+print('reslut_dir:{}'.format(result_dir))
+
+trainer.run()
---
+++ src/vdcnn_multi_4.py
@@ -0,0 +1,153 @@
+import chainer
+import chainer.functions as F
+from chainer import function, initializers
+import chainer.links as L
+from chainer import cuda
+import numpy as np
+from kmaxpooling1d import k_max_pooling_1d
+from init import Uniform
+
+
+class ConvBlock(chainer.Chain):
+
+    def __init__(self, ch_size, out_size):
+        initialW = initializers.HeUniform()
+        super(ConvBlock, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.ConvolutionND(
+                1, ch_size, ch_size, 3, 1, 1,
+                initialW=initialW,
+                initial_bias=initializers.Uniform(np.sqrt(6/(ch_size*3))))
+            self.bn1 = L.BatchNormalization(ch_size, initial_gamma=Uniform(1.0))
+            self.conv2 = L.ConvolutionND(
+                1, ch_size, out_size, 3, 1, 1,
+                initialW=initialW,
+                initial_bias=initializers.Uniform(np.sqrt(6/(ch_size*3))))
+            self.bn2 = L.BatchNormalization(out_size, initial_gamma=Uniform(1.0))
+
+    def __call__(self, x):
+        h = F.relu(self.bn1(self.conv1(x)))
+        h = self.bn2(self.conv2(h))
+        return F.relu(h)
+
+    
+class BuildBlock(chainer.ChainList):
+    
+    def __init__(self, layer, ch_size, out_size):
+        super(BuildBlock, self).__init__()
+        for i in range(layer - 1):
+            self.add_link(ConvBlock(ch_size, ch_size))
+        self.add_link(ConvBlock(ch_size, out_size))
+    
+    def __call__(self, x):
+        for f in self.children():
+            x = f(x)
+        return x
+
+    
+class MultiVDCNN(chainer.Chain):
+    
+    def __init__(self, emb_dim, n_out_1, n_out_2, n_out_3, n_out_4, depth=[2,2,2,2]):
+        super(MultiVDCNN, self).__init__()
+        with self.init_scope():
+            self.embed = L.EmbedID(
+                emb_dim, 16, initializers.Normal(), ignore_label=-1)
+            self.conv1 = L.ConvolutionND(
+                1, 16, 64, 3, 1, 1,
+                initialW=initializers.HeUniform(),
+                initial_bias=initializers.Uniform(np.sqrt(6/(16*3))))
+            self.cb2 = BuildBlock(depth[0], 64, 128)
+            self.cb3 = BuildBlock(depth[1], 128, 256)
+            self.cb4 = BuildBlock(depth[2], 256, 512)
+            self.cb5 = BuildBlock(depth[3], 512, 512)
+            self.fc6 = L.Linear(
+                4096, 2048,
+                initialW=initializers.Uniform(1 / np.sqrt(4096)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(4096)))
+            self.fc7 = L.Linear(
+                2048, 2048,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+            self.fc8_1 = L.Linear(
+                2048, n_out_1,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+            self.fc8_2 = L.Linear(
+                2048, n_out_2,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+            self.fc8_3 = L.Linear(
+                2048, n_out_3,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+            self.fc8_4 = L.Linear(
+                2048, n_out_4,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+    
+    def __call__(self, x):
+        h = self.embed(x)
+        h = h.transpose(0,2,1)
+        h = self.conv1(h)
+        h = self.cb2(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb3(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb4(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb5(h)
+        h = k_max_pooling_1d(h, 8)
+        h = F.relu(self.fc6(h))
+        h = F.relu(self.fc7(h))
+        o1 = self.fc8_1(h)
+        o2 = self.fc8_2(h)
+        o3 = self.fc8_3(h)
+        o4 = self.fc8_4(h)
+        out = F.concat([o1, o2, o3, o4], axis=1)
+        return out
+
+    
+class VDCNN(chainer.Chain):
+    
+    def __init__(self, emb_dim, n_out, depth=[2,2,2,2]):
+        super(VDCNN, self).__init__()
+        with self.init_scope():
+            self.embed = L.EmbedID(
+                emb_dim, 16, initializers.Normal(), ignore_label=-1)
+            self.conv1 = L.ConvolutionND(
+                1, 16, 64, 3, 1, 1,
+                initialW=initializers.HeUniform(),
+                initial_bias=initializers.Uniform(np.sqrt(6/(16*3))))
+            self.cb2 = BuildBlock(depth[0], 64, 128)
+            self.cb3 = BuildBlock(depth[1], 128, 256)
+            self.cb4 = BuildBlock(depth[2], 256, 512)
+            self.cb5 = BuildBlock(depth[3], 512, 512)
+            self.fc6 = L.Linear(
+                4096, 2048,
+                initialW=initializers.Uniform(1 / np.sqrt(4096)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(4096)))
+            self.fc7 = L.Linear(
+                2048, 2048,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+            self.fc8 = L.Linear(
+                2048, n_out,
+                initialW=initializers.Uniform(1 / np.sqrt(2048)),
+                initial_bias=initializers.Uniform(1 / np.sqrt(2048)))
+    
+    def __call__(self, x):
+        h = self.embed(x)
+        h = h.transpose(0,2,1)
+        h = self.conv1(h)
+        h = self.cb2(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb3(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb4(h)
+        h = F.max_pooling_nd(h, 3, 2, 1, cover_all=False)
+        h = self.cb5(h)
+        h = k_max_pooling_1d(h, 8)
+        h = F.relu(self.fc6(h))
+        h = F.relu(self.fc7(h))
+        h = self.fc8(h)
+        return h
