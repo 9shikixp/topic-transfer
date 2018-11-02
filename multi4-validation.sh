f0b2894250379fe5cf1e7af0c2353296e8afbea1
moriya <c0115334ef@edu.teu.ac.jp>
2018-11-02 10:07:10 +0000

    add script for multi4 transfer

---
+++ multi4-validation.sh
@@ -0,0 +1,14 @@
+cd ~/work/topic-transfer/src/
+case $HOSTNAME in
+  "clover11" ) DATA=yelpf;;
+  "clover12" ) DATA=db_pedia ;;
+  "denso" ) DATA=ag_news ;;
+esac
+echo mode=$DATA gpu_id=$1
+for i in `seq $1 2 4`
+do
+  echo python topic-train-multi-4.py -m $DATA -g $1 -d 17 -v $i
+  python topic-train-multi-4.py -m $DATA -g $1 -d 17 -v $i
+  echo python transfer-multi-4.py -m $DATA -g $1 -d 17 -v $i
+  python transfer-multi-4.py -m $DATA -g $1 -d 17 -v $i
+done
