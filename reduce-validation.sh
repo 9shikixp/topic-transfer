35b986589d9121caa9d604ed3ba0956e251db5c2
moriya <c0115334ef@edu.teu.ac.jp>
2018-11-07 06:36:36 +0000

    add scratch reduce

--- reduce-validation.sh
+++ reduce-validation.sh
@@ -11,3 +11,9 @@ do
   echo python transfer-soft-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
   python transfer-soft-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
 done
+START=$((($1+1)%2+1))
+for i in `seq $START 2 5`
+do
+  echo python train-scratch-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
+  python train-scratch-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
+done
