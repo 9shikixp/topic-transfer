eab0bb7aa8f888a4df9a04e68dee1dbdb50bcef6
moriya <c0115334ef@edu.teu.ac.jp>
2018-11-07 03:56:09 +0000

    add reduce valid(reduce=[4,16,64,256,1024])

---
+++ reduce-validation.sh
@@ -0,0 +1,13 @@
+cd ~/work/topic-transfer/src/
+case $HOSTNAME in
+  "clover11" ) DATA=yelpf;;
+  "clover12" ) DATA=db_pedia ;;
+  "denso" ) DATA=ag_news ;;
+esac
+echo mode=$DATA gpu_id=$1
+START=$(($1+1))
+for i in `seq $START 2 5`
+do
+  echo python transfer-soft-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
+  python transfer-soft-reduce.py -m $DATA -g $1 -d 17 -v 0 -r $((4**$i))
+done
